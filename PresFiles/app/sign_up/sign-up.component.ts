377ae0c53f3fd5bc5c4c50754e4414f58e897133
Nicholas Moran <Nicholas Moran>
2017-01-15 21:19:05 -0600

    yas

--- PresFiles/app/sign_up/sign-up.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
-@Component({
-    selector: 'Sign-up',
-    templateUrl: './app/sign_up/sign-up.component.html',
-    styleUrls: ['./app/sign_in/sign-in.component.css']
-})
-export class SignUpComponent {}
